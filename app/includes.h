ddd03a7e8fea5d8bd85ad88b2cc42ae7b0098f02
Khanh Tran <dkhanh1702@gmail.com>
2018-09-28 22:31:03 +0700

    Finish porting uCOSII on Window and successfully built on Eclipse

---
+++ app/includes.h
@@ -0,0 +1,27 @@
+/*
+*********************************************************************************************************
+*                                                uC/OS-II
+*                                          The Real-Time Kernel
+*
+*                              (c) Copyright 1992-2004, Micrium, Weston, FL
+*                                           All Rights Reserved
+*
+*                                           MASTER INCLUDE FILE
+*********************************************************************************************************
+*/
+
+#include    <stdio.h>
+#include    <string.h>
+#include    <ctype.h>
+#include    <stdlib.h>
+
+//#include    "mmsystem.h"
+#include    <app_cfg.h>
+#include    <ucos_ii.h>
+#include    "pc.h"
+
+
+#if OS_VIEW_MODULE
+#include    <os_viewc.h>
+#include    <os_view.h>
+#endif
